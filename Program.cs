b8019a030634c190fa66dbbe577550c45d35f3ee
coskunrumeysa <70705198+coskunrumeysa@users.noreply.github.com>
2022-05-27 16:48:59 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿//Beni Yaratan Elbet Yolumu Gösterir .Şuara 78
+ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi;
+            Console.WriteLine("Please enter a number:");
+            sayi = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Entried number :" + " " + sayi);
+            Console.Read();
+        }
+    }
+}
